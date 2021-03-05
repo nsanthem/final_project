7dd56c61881e09ee9c448eb590da2dd0ab83dabf
Cindy Wang <cindy.wang2@kellogg.northwestern.edu>
2021-03-05 10:57:06 -0600

    added admin.js

---
+++ admin.js
@@ -0,0 +1,24 @@
+firebase.auth().onAuthStateChanged(async function(user) {
+    if (user) {
+      // Signed in
+      console.log('signed in')
+    } else {
+      // Signed out
+      console.log('signed out')
+  
+      // Initializes FirebaseUI Auth
+      let ui = new firebaseui.auth.AuthUI(firebase.auth())
+  
+      // FirebaseUI configuration
+      let authUIConfig = {
+        signInOptions: [
+          firebase.auth.EmailAuthProvider.PROVIDER_ID
+        ],
+        signInSuccessUrl: 'admin.html'
+      }
+  
+      // Starts FirebaseUI Auth
+      ui.start('.sign-in-or-sign-out', authUIConfig)
+    }
+  })
+  
