628e41bc7f28147e6893074959fcccfbebd97a61
Cindy Wang <cindy.wang2@kellogg.northwestern.edu>
2021-03-05 12:03:53 -0600

    added logout

--- admin.js
+++ admin.js
@@ -1,7 +1,14 @@
 firebase.auth().onAuthStateChanged(async function(user){
     if (user){
     // Signed in
-    document.querySelector('.sign-in-or-sign-out').innerHTML = '<h3>Hi there USER</h3><a href="#" class="sign-out text-green-500-underline">Sign Out</a>'
+    document.querySelector('.sign-in-or-sign-out').innerHTML = '<h3>Hi there USER</h3> <a href="#" class="sign-out text-green-500-underline">Sign Out</a>'
+
+    document.querySelector('.sign-out').addEventListener('click', function(event){
+        event.preventDefault()
+        firebase.auth().signOut()
+        document.location.href = 'admin.html'
+    })
+
     console.log('signed in')
 
     }else{
