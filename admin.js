856a1eda0f4fd933b09e1c0f138e3c392bbff334
Cindy Wang <cindy.wang2@kellogg.northwestern.edu>
2021-03-05 11:42:56 -0600

    added sign in/sign out functionality

--- admin.js
+++ admin.js
@@ -1,24 +1,25 @@
-firebase.auth().onAuthStateChanged(async function(user) {
-    if (user) {
-      // Signed in
-      console.log('signed in')
-    } else {
-      // Signed out
-      console.log('signed out')
+firebase.auth().onAuthStateChanged(async function(user){
+    if (user){
+    // Signed in
+    document.querySelector('.sign-in-or-sign-out').innerHTML = '<h3>Hi there USER</h3><a href="#" class="sign-out text-green-500-underline">Sign Out</a>'
+    console.log('signed in')
+
+    }else{
+    // Signed out
+    console.log('signed out')
+
+    // Initializes FirebaseUI Auth
+    let ui = new firebaseui.auth.AuthUI(firebase.auth())
   
-      // Initializes FirebaseUI Auth
-      let ui = new firebaseui.auth.AuthUI(firebase.auth())
-  
-      // FirebaseUI configuration
-      let authUIConfig = {
-        signInOptions: [
+    // FirebaseUI configuration
+    let authUIConfig = {
+    signInOptions: [
           firebase.auth.EmailAuthProvider.PROVIDER_ID
         ],
-        signInSuccessUrl: 'admin.html'
-      }
-  
-      // Starts FirebaseUI Auth
-      ui.start('.sign-in-or-sign-out', authUIConfig)
+    signInSuccessUrl: 'admin.html'
+    }
+    // Starts FirebaseUI Auth
+    ui.start('.sign-in-or-sign-out', authUIConfig)
+
     }
   })
-  
