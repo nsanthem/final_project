45cdfa98879639973da3e8772716452d00843e44
Cindy Wang <cindy.wang2@kellogg.northwestern.edu>
2021-03-13 18:03:26 -0600

    set user details

--- admin.js
+++ admin.js
@@ -1,37 +1,45 @@
 firebase.auth().onAuthStateChanged(async function(user){
+  let db = firebase.firestore()
 
-    let db = firebase.firestore()
     if (user){
-    // Signed in
-    document.querySelector('.sign-in-or-sign-out').innerHTML = '<h3>Hi there USER</h3> <a href="#" class="sign-out text-green-500-underline">Sign Out</a>'
+      // Set User Details
+      db.collection('users').doc(user.uid).set({
+        name: user.displayName,
+        email: user.email
+      })
 
-    document.querySelector('.sign-out').addEventListener('click', function(event){
-        event.preventDefault()
-        firebase.auth().signOut()
-        document.location.href = 'admin.html'
-    })
+      // Signed in
+      document.querySelector('.sign-in-or-sign-out').innerHTML = `<h3>Hi ${user.displayName}!</h3> <a href="#" class="sign-out text-green-500 underline">Sign Out</a>`
 
-    console.log('signed in')
+      document.querySelector('.sign-out').addEventListener('click', function(event){
+          event.preventDefault()
+          firebase.auth().signOut()
+          document.location.href = 'admin.html'
+      })
+      console.log('signed in') 
+
+      //Edit quantity
+      document.querySelector('.editPrice').addEventListener()
+      
     }else{
+      //Hide data
+      document.querySelector('.admin-view').classList.add('hidden')
 
-    //Hide data
-    document.querySelector('.admin-view').classList.add('hidden')
+      // Signed out
+      console.log('signed out')
 
-    // Signed out
-    console.log('signed out')
+      // Initializes FirebaseUI Auth
+      let ui = new firebaseui.auth.AuthUI(firebase.auth())
+    
+      // FirebaseUI configuration
+      let authUIConfig = {
+      signInOptions: [
+            firebase.auth.EmailAuthProvider.PROVIDER_ID
+          ],
+      signInSuccessUrl: 'admin.html'
+      }
+      // Starts FirebaseUI Auth
+      ui.start('.sign-in-or-sign-out', authUIConfig)
 
-    // Initializes FirebaseUI Auth
-    let ui = new firebaseui.auth.AuthUI(firebase.auth())
-  
-    // FirebaseUI configuration
-    let authUIConfig = {
-    signInOptions: [
-          firebase.auth.EmailAuthProvider.PROVIDER_ID
-        ],
-    signInSuccessUrl: 'admin.html'
-    }
-    // Starts FirebaseUI Auth
-    ui.start('.sign-in-or-sign-out', authUIConfig)
-
-    }
+      }
   })
