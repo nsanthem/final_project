6b0e1eedeea44cd97040e0ef95d406d45e27b723
Cindy Wang <cindy.wang2@kellogg.northwestern.edu>
2021-03-16 17:47:28 -0500

    tried to add js to admin; not totally working...

--- admin.js
+++ admin.js
@@ -18,41 +18,54 @@ firebase.auth().onAuthStateChanged(async function(user){
           document.location.href = 'admin.html'
       })
       // End of Sign in
-
-      // Start JS function to pull products from firebase and display on admin.html
-      async function renderProduct(productName, productPrice, productQuantity, productUrl) {
-              document.querySelector('.products').insertAdjacentHTML('beforeend',`
-                <div class="text-white">
-                <div class="productListing mx-4">
-                <img src=${productUrl}>
-                <p class="font-bold mt-4 text-xl font-serif md:text-2xl">${productName}</p>
-                <p class="font-bold">Price: $${productPrice}</p>
-                <p class="font-bold mb-4">Quantity: ${productQuantity}</p>
-                </div>
-                </div>
-              `)
-      }
-
-      window.addEventListener('DOMContentLoaded', async function(event) {
+    
+      // start js function to pull products from firebase and display on products html
+        async function renderProduct(productName, productPrice, productQuantity, productUrl) {
+          document.querySelector('.products').insertAdjacentHTML('beforeend',`
+          <div class="text-white">
+              <div class="productListing mx-4">
+                  <img src=${productUrl}>
+                  <p class="font-bold mt-4 text-xl font-serif md:text-2xl">${productName}</p>
+                  <p class="font-bold">Price: $${productPrice}</p>
+                  <p class="font-bold mb-4">Quantity: ${productQuantity}</p>
+                  <div class ="productDetails">
+                        <form class="form">
+                            <div class="price">
+                                <p class="mt-4 priceLabel">Price</p>
+                                <input type="number" id="price" name="price" min="00.00" step="00.01" class="editPrice text-black" value="00.00">
+                            </div>
+                            <div class="quantity mt-8">
+                                <p class="mt-4 quantityLabel">Qty</p>
+                                <input type="number" id="quantity" name="quantity" min="0" step="1" class="editQuantity text-black" value="1">
+                            </div>
+                            <div class="mt-8 button">
+                                <input type="submit" value="Update" class="updateButton bg-green-400 text-white py-2 px-8 hover:bg-green-600 rounded-xl">
+                            </div>
+                  </div>
+              </div>
+          </div>
+        `)
+        }
+        window.addEventListener('DOMContentLoaded', async function(event) {
         event.preventDefault()
+
         let querySnapshot = await db.collection('products').get()
         let products = querySnapshot.docs
 
         console.log(products)
 
         for (let i=0; i<products.length; i++) { 
-          let productData = products[i].data()
-          let productName = productData.itemName
-          let productPrice = productData.priceData
-          let productQuantity = productData.quantityData
-          let productUrl = productData.imgUrl
-          
-          renderProduct(productName, productPrice, productQuantity, productUrl)
+            let productData = products[i].data()
+            let productName = productData.itemName
+            let productPrice = productData.priceData
+            let productQuantity = productData.quantityData
+            let productUrl = productData.imgUrl
+            renderProduct(productName, productPrice, productQuantity, productUrl)
+        }       
 
-        }
+        })
+        // end javascript function to display products
 
-      })
-      // end javascript function to display products
       
 
       //Listen for form submission and set new quantity & price
