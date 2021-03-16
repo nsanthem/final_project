376640f6fa93d4b1a7b19125e2fb0eb1d3a419c9
Cindy Wang <cindy.wang2@kellogg.northwestern.edu>
2021-03-16 16:54:32 -0500

    Edited html to make products lay out in grid

--- products.js
+++ products.js
@@ -4,14 +4,13 @@ let db = firebase.firestore()
 // start js function to pull products from firebase and display on products html
 async function renderProduct(productName, productPrice, productQuantity, productUrl) {
   document.querySelector('.products').insertAdjacentHTML('beforeend',`
-  <div class="m-12 md:flex text-white text-md text-left">
-      <div>
-          <img class="w-full" src=${productUrl}></p>
+  <div class="text-white">
+      <div class="productListing mx-4">
+          <img src=${productUrl}>
+          <p class="font-bold mt-4 text-xl md:text-2xl">${productName}</p>
+          <p class="font-bold">Price: $${productPrice}</p>
+          <p class="font-bold mb-4">Quantity: ${productQuantity}</p>
       </div>
-      <div>
-          <p class="font-bold">${productName}</p>
-          <p class=" font-bold">Price: $${productPrice}</p>
-          <p class=" font-bold">Quantity: ${productQuantity}</p>
   </div>
 `)
 }
@@ -24,14 +23,16 @@ let products = querySnapshot.docs
 
 console.log(products)
 
-for (let i=0; i<products.length; i++) {  
+for (let i=0; i<products.length; i++) { 
     let productData = products[i].data()
     let productName = productData.itemName
     let productPrice = productData.priceData
     let productQuantity = productData.quantityData
     let productUrl = productData.imgUrl
-
     renderProduct(productName, productPrice, productQuantity, productUrl)
+
 }
+
 })
+
 // end javascript function to display products
