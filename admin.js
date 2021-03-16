26e4ab75f4e79b26f31f8b9021f818459408ea7e
Cindy Wang <cindy.wang2@kellogg.northwestern.edu>
2021-03-16 17:39:00 -0500

    Archived old pages

--- admin.js
+++ admin.js
@@ -18,29 +18,64 @@ firebase.auth().onAuthStateChanged(async function(user){
           document.location.href = 'admin.html'
       })
       // End of Sign in
+
+      // Start JS function to pull products from firebase and display on admin.html
+      async function renderProduct(productName, productPrice, productQuantity, productUrl) {
+              document.querySelector('.products').insertAdjacentHTML('beforeend',`
+                <div class="text-white">
+                <div class="productListing mx-4">
+                <img src=${productUrl}>
+                <p class="font-bold mt-4 text-xl font-serif md:text-2xl">${productName}</p>
+                <p class="font-bold">Price: $${productPrice}</p>
+                <p class="font-bold mb-4">Quantity: ${productQuantity}</p>
+                </div>
+                </div>
+              `)
+      }
+
+      window.addEventListener('DOMContentLoaded', async function(event) {
+        event.preventDefault()
+        let querySnapshot = await db.collection('products').get()
+        let products = querySnapshot.docs
+
+        console.log(products)
+
+        for (let i=0; i<products.length; i++) { 
+          let productData = products[i].data()
+          let productName = productData.itemName
+          let productPrice = productData.priceData
+          let productQuantity = productData.quantityData
+          let productUrl = productData.imgUrl
+          
+          renderProduct(productName, productPrice, productQuantity, productUrl)
+
+        }
+
+      })
+      // end javascript function to display products
       
 
       //Listen for form submission and set new quantity & price
-      document.querySelector('.updateButton').addEventListener('click', async function(event){
-        event.preventDefault()
+      // document.querySelector('.updateButton').addEventListener('click', async function(event){
+      //   event.preventDefault()
 
-        let price = document.querySelector('.editPrice').value
-        let quantity = document.querySelector('.editQuantity').value
-        console.log(`submitted new price of ${price} and ${quantity}!`)
+      //   let price = document.querySelector('.editPrice').value
+      //   let quantity = document.querySelector('.editQuantity').value
+      //   console.log(`submitted new price of ${price} and ${quantity}!`)
         
-        //Adding form information to firebase
-        let docRef = await db.collection('products').add({
+      //   //Adding form information to firebase
+      //   let docRef = await db.collection('products').add({
 
-          priceData: price,
-          quantityData: quantity
-        })
-        // End of adding quantity and price data to firebase
+      //     priceData: price,
+      //     quantityData: quantity
+      //   })
+      //   // End of adding quantity and price data to firebase
 
-        //Remove old entry
-        let thisherId = docRef.id
-        console.log(`new input with ID ${thisherId} created`)
+      //   //Remove old entry
+      //   let thisherId = docRef.id
+      //   console.log(`new input with ID ${thisherId} created`)
 
-      })
+      // })
       // End of form submission
 
 
